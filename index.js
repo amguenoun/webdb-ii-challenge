ca1a3637dc1d81d2af165e38a8e40b21e8a235ab
Ahmad Guenoun <amguenoun@gmail.com>
2019-11-22 17:18:58 -0500

    created get and post endpoints for the api

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const express = require('express');
+require('dotenv').config();
+
+const server = require('./server');
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+})
